04fa8366ab2e78b0eb1db4d8c254deafe6fd2c62
0xs1riu5 <39816462+0xs1riu5@users.noreply.github.com>
2018-08-25 08:56:19 +0800

    Update schema.sql

--- zzcms/8.3/mysql/schema.sql
+++ zzcms/8.3/mysql/schema.sql
@@ -18,7 +18,9 @@
 --
 -- Table structure for table `zzcms_about`
 --
-CREATE DATABASE zzcms  CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS zzcms DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+use zzcms;
+
 DROP TABLE IF EXISTS `zzcms_about`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
